39a702a855208cde6428f61717da78aebdf58136
hendri54 <hendricks.lutz@gmail.com>
2017-10-02 14:38:24 -0400

    classes

--- econ720/class_schedule_720.m
+++ econ720/class_schedule_720.m
@@ -164,7 +164,7 @@ topicV{i1} = markdownLH.SubTopic({'[Optimal control](ih2/OptControl_SL.pdf)'},  
 
 i1 = i1 + 1;
 topicV{i1} = markdownLH.SubTopic({'[The growth model](ih2/Ramsey_SL.pdf)', ...
-   '[Competitive equilibrium](ih2/ih2_equil_sl.pdf'});
+   '[Competitive equilibrium](ih2/ih2_equil_sl.pdf)'});
 
 i1 = i1 + 1;
 if 0
