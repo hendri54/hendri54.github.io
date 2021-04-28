4586188425d551dce94fcf0a2083fc6defa6114d
hendri54 <hendricksl@protonmail.com>
2021-04-28 16:02:06 -0400

    520

--- econ890/julia/examples/infiltrator.jl
+++ econ890/julia/examples/infiltrator.jl
@@ -1,5 +1,9 @@
 # Example to demonstrate Infiltrator
 
+# const ExampleDir = "/Users/lutz/Documents/data/web/professional/docs/econ890/julia/examples";
+# using Pkg
+# Pkg.add("Infiltrator");
+
 using Infiltrator
 
 function infil_test()
